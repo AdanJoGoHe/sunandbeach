a2c1d1ceef71c4aacc1ea2219d00f7533ec23d14
ManuMonLeo <153329302+ManuMonLeo@users.noreply.github.com>
2023-12-10 10:46:26 +0000

    4-candelabro ta finiquitao (#5)

---
+++ src/main/java/com/serex/beachandsun/items/Candelabro.java
@@ -0,0 +1,10 @@
+package com.serex.beachandsun.items;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+
+public class Candelabro extends Item {
+    public Candelabro(FabricItemSettings fabricItemSettings) {
+        super(fabricItemSettings);
+    }
+}
